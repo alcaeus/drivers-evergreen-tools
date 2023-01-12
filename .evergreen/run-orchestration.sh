f5b00250970c5fdc5174471fa607bbab6caa1448
Andreas Braun <git@alcaeus.org>
2023-01-12 13:57:09 +0100

    Use mongosh for stable API tests

--- .evergreen/run-orchestration.sh
+++ .evergreen/run-orchestration.sh
@@ -143,5 +143,5 @@ EOT
 
 # Set the requireApiVersion parameter
 if [ ! -z "$REQUIRE_API_VERSION" ]; then
-  mongo $URI $MONGO_ORCHESTRATION_HOME/require-api-version.js
+  mongosh $URI $MONGO_ORCHESTRATION_HOME/require-api-version.js
 fi
